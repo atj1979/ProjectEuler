99a759b49df0c8b68416d301b373bb645fa2b73b
Anthony Johnson <atj1979@gmail.com>
2015-06-05 11:47:12 -0700

    (add) LargestPalindromicProduct

---
+++ LargestPalindromicProduct.js
@@ -0,0 +1,26 @@
+// A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+
+// Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+function isPalindromic (number){
+	
+
+}
+
+function findLargestPalinProduct (numOfDigits){
+	//get two of the biggest #
+	var bigNum = ""; 
+	for ( var i = 0; i < numOfDigits; i++ ){
+		bigNum += 9;
+	}
+	bigNum = parseInt(bigNum);
+
+
+
+
+
+
+}
+
+findLargestPalinProduct(3);
