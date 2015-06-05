67d23203a6be1dff9731b4e18ec6219e076eef00
Anthony Johnson <atj1979@gmail.com>
2015-06-04 22:28:22 -0700

    (completed) Largest Prime factor and refactored to increase speed reduce lines - friendly competition with someone

--- LargestPrimeFactor.js
+++ LargestPrimeFactor.js
@@ -1,15 +1,15 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 
-//prime number are numbers that are tested between starting at 2 thru num* 0.25 or 0.5 thru 0.25
-
-
-function isPrime (num){
-	var start = Math.ceil(num/2);
-	var end = Math.floor(num * .25);
+function isPrime (num, previousPrimes){
+	//anything less than 3 is a gimmie
 	if (num <= 3){ return true; }
 	
-	for (var i = start; i > end; i--){
-		// console.log(i + " | " + start + " | " + end);
+	//skip previous prime numbers
+	if (previousPrimes !== undefined){
+		if (previousPrimes[(previousPrimes.length-1).toString()])
+		return true;
+	}
+	for (var i = 2; i <= Math.sqrt(num); i++){
 		if (num % i === 0){
 			return false;
 		}  
@@ -18,18 +18,32 @@ function isPrime (num){
 }
 
 function LargestPrimeFactors(num){
-	var start = Math.ceil(num/2);
-	var end = Math.floor(num * .25);
-	// console.log(start + " | " + end);
-	for (var i = start; i > end; i--){
-		// console.log("checking " + i);
-		if (isPrime(i) && num % i === 0){
-			return i;
+	var end = Math.sqrt(num);
+	var primesChecked = {};
+	var largestPrime = 0;
+	var numi = 1;
+	
+	for (var i = 2; i < end; i++){ // build a list of all the prime numbers from 0 to sqrt(num)
+		if (num % i === 0){ 
+			if (isPrime(i, primesChecked)){ // check to see if the number is prime, send along the prime list
+				primesChecked[i.toString()]=true; // if the number is prime - add it to the seive
+				largestPrime = i > largestPrime ? i : largestPrime; // check to see if the new number is the largest
+			}
+			numi = num / i;  // calculate the other factor of the input number
+			if (isPrime(numi, primesChecked)){ // check to see if the number is prime, send along the prime list
+				primesChecked[numi.toString()]=true; // if the number is prime - add it to the seive
+				largestPrime = numi > largestPrime ? numi : largestPrime; // check to see if the new number is the largest
+			}
 		}
 	}
-	return "no Answer";
+	return largestPrime;
 }
 
+	
 console.log(LargestPrimeFactors(600851475143));
+
+
+// The largest increase in speed was adding in the ternarys in place of if statements.
+
 
 
